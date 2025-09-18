2d14545cf34d352e19e51b031865560cb9d090a0
Akash97855 <akashshah0815@gmail.com>
2025-09-18 15:12:38 +0530

    updated synchronous.js and jsasync.js

--- js/synchronous.js
+++ js/synchronous.js
@@ -13,9 +13,11 @@
 //login()
 //getdata()
 //display data()
-function waitfortwoseconds()
+
+//callback hell...
+/*function waitfortwoseconds()
 {
-    const ms=new Date().getTime()+1000;
+    const ms=new Date().getTime()+8000;
     while(new Date().getTime()<ms);
 }
 function register()
@@ -48,5 +50,87 @@ sendemail();
 login();
 getData();
 displayData();
-console.log("other Application")
+console.log("other Application")*/
+// function waitfortwoseconds()
+// {
+//     const ms=new Date().getTime()+8000;
+//     while(new Date().getTime()<ms);
+// }
+// function register()
+// {
+//     waitfortwoseconds();
+//     console.log("register end");
+// }
+// function sendemail()
+// {
+//     waitfortwoseconds();
+//     console.log("email end");
+// }
+// function login()
+// {
+//     waitfortwoseconds();
+//     console.log(" login end");
+// }
+// function getData()
+// {
+//     waitfortwoseconds();
+//     console.log("data end");
+// }
+// function displayData()
+// {
+//     waitfortwoseconds();
+//     console.log("Display end");
+// }
+// register();
+// sendemail();
+// login();
+// getData();
+// displayData();
+// console.log("other Application")
+
+//callback hell.......
+ 
+function register(cb){
+    setTimeout(()=>{
+        console.log("register end");
+        cb();
+    },2000)
+}
+function sendemail(cb){
+    setTimeout(()=>{
+        console.log("email end");
+        cb();
+    },2000)
+}
+function login(cb){
+    setTimeout(()=>{
+        console.log("login end");
+        cb();
+    },2000)
+}
+function getData(cb){
+    setTimeout(()=>{
+        console.log("data end");
+        cb();
+    },2000)
+}
+function displayData(){
+    setTimeout(()=>{
+        console.log("display end");
+    },2000)
+}
+
+register(()=>{
+    sendemail(()=>{
+        login(()=>{
+            getData(()=>{
+                displayData();
+            })
+        })
+    })
+})
+
+console.log("other Applications");
+
+
 
